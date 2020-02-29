7335ecf0e98b8566a42f72e9b1d40a8351e1070d
Nooshin Esmaeilzadeh <nooshin@192-168-1-104.tpgi.com.au>
2020-02-29 21:52:21 +1100

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,6 +1,45 @@
-let cardOne = "queen";
-let cardTwo = "queen";
-let cardThree = "king";
-let cardFour = "king";
+let cards = [
+  {
+    rank: "queen",
+    suit: "hearts",
+    cardImage: "images/queen-of-hearts.png"
+  },
+  {
+    rank: "queen",
+    suit: "diamonds",
+    cardImage: "images/queen-of-diamonds.png"
+  },
+  {
+    rank: "king",
+    suit: "hearts",
+    cardImage: "images/king-of-hearts.png"
+  },
+  {
+    rank: "king",
+    suit: "diamonds",
+    cardImage: "images/king-of-diamonds.png"
+  }
+];
 
-console.log("User flipped " + cardOne);
+let cardsInPlay = [];
+
+let checkForMatch = function() {
+  if (cardsInPlay[0] === cardsInPlay[1]) {
+    console.log("You found a match!");
+  } else {
+    console.log("Sorry, try again.");
+  }
+};
+
+let flipCard = function(cardId) {
+  if (cardsInPlay.length === 2) {
+    checkForMatch();
+  }
+  console.log("User flipped" + " " + cards[cardId].rank);
+  cardsInPlay.push(cards[cardId].rank);
+  console.log(cards[cardId].cardImage);
+  console.log(cards[cardId].suit);
+};
+
+flipCard(0);
+flipCard(2);
